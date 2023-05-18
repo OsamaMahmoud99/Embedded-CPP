4d78ee211b03f2fdc881e4db0cc30ab3ff7664ea
osama <gad153879@gmail.com>
2023-05-18 19:28:58 +0200

    ledclass

--- Udemy - Embedded Systems Object-Oriented Programming in C and C++/LedClass_in_c/Led.c
+++ Udemy - Embedded Systems Object-Oriented Programming in C and C++/LedClass_in_c/Led.c
@@ -3,8 +3,8 @@
 
 void Led_ctor(Led_Type* const me, LedColor_Type _color, LedState_Type _state){
 
-//	me->color = _color;
-	//me->state = _state;
+	me->color = _color;
+	me->state = _state;
 
 }
 void Led_setState(Led_Type* const me , LedState_Type _state){
